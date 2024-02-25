95895b7767599a72446c37816353ee01a277c3d0
bhanudatsinhjhala <jhalabhanudattsinh4090@gmail.com>
2024-02-25 14:38:52 +0530

    feat(browser-markdown): Add the markdown-to-jsx to render data in component.
    - Add the markdown-to-jsx to render data in component
    - Add styled-components to create component with custom css for h1 and h2 tag

---
+++ browser-markdown/src/components/markdown-template.tsx
@@ -0,0 +1,12 @@
+import MarkdownConfig from "./MarkdownConfig";
+
+function Markdown() {
+  return (
+    <div>
+      <MarkdownConfig />
+      <p>Markdown</p>
+    </div>
+  );
+}
+
+export default Markdown;
